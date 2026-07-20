9edac908ec9e9eabac8d05c728f6c71069af865b
NamesAreHard Laptop <109428297+NamesAreHard6600@users.noreply.github.com>
2026-07-20 11:45:43 -0700

    Get Angry and Calm Down

---
+++ NorthmanCode/Cards/Common/CalmDown.cs
@@ -0,0 +1,25 @@
+﻿using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using BaseLib.Utils;
+
+namespace Northman.NorthmanCode.Cards.Common;
+
+
+public class CalmDown : NorthmanCard  
+{
+    public CalmDown() : base(0, CardType.Skill, CardRarity.Common, TargetType.Self)
+    {
+        WithAnger(-2);
+        WithCards(1);
+        WithKeyword(CardKeyword.Exhaust, UpgradeType.Remove);
+        
+    }
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext ctx,
+        CardPlay cardPlay)
+    {
+        await AdjustAnger(ctx, cardPlay);
+        await CommonActions.Draw(this, ctx);
+    }
+}
